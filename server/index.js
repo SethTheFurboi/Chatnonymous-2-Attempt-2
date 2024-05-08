85d6e42e55724645fc11eb533295cee516bdb67e
= <s3tha@icloud.com>
2024-05-07 20:20:20 -0500

    added a placeholder backend

--- server/index.js
+++ server/index.js
@@ -3,10 +3,23 @@ const app = express()
 const cors = require("cors")
 const http = require('http').Server(app);
 const PORT = 4000
+const PORT2 = 4001
 const socketIO = require('socket.io')(http, {
     cors: {
         origin: "http://localhost:3000"
     }
+});
+
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
 app.use(cors())
@@ -34,6 +47,33 @@ socketIO.on('connection', (socket) => {
       socket.disconnect()
     });
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
+
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
+
+  db.once('open', () => {
+    app.listen(PORT2, () => {
+      console.log(`API server running on port ${PORT2}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT2}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
 
 app.get("/api", (req, res) => {
   res.json({message: "Hello"})
